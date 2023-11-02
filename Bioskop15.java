15734763fe10677c6adb37d2491cff7a5fb5412b
Lyrafaiqahh <lyrafaiqah@gmail.com>
2023-11-02 14:22:02 +0700

    PERCOBAAN 1 JOBSHEET 10 NO.6

--- Bioskop15.java
+++ Bioskop15.java
@@ -24,7 +24,12 @@ public class Bioskop15 {
     for (int i = 0; i< penonton.length; i++) {
         System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
     }
+    System.out.println(penonton.length);
 
-    
+    for (String[] barisPenonton : penonton) {
+        System.out.println("Panjang baris: " + barisPenonton.length);
+    }
+
+
     }
 }
